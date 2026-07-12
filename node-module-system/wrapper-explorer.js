8cf79e272199c89a87d43655272667ba8944de9a
sudhanshu ranjan <166327067+Mithu699@users.noreply.github.com>
2026-07-12 12:43:25 +0530

    Add files via upload

---
+++ node-module-system/wrapper-explorer.js
@@ -0,0 +1,10 @@
+console.log('node module wrapper demo');
+
+
+console.log('__filename in explorer', __filename);
+console.log('__dirname in explorer', __dirname);
+
+
+module.exports.greet = function(name){
+    console.log(` Hello ${name}`)
+}
