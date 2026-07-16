4336ac69079a81e42fdb91d5413158cb9a19f578
sudhanshu ranjan <166327067+Mithu699@users.noreply.github.com>
2026-07-16 20:55:57 +0530

    Delete node-module-system directory

--- node-module-system/wrapper-demo.js
+++
@@ -1,7 +0,0 @@
-const wrapperExplorer = require("./wrapper-explorer");
-
-console.log(`in demo wrapper demo.js file `);
-console.log("__filename in demo", __filename);
-console.log("__dirname in demo", __dirname);
-
-wrapperExplorer.greet("sudhanshu ranjan ");
