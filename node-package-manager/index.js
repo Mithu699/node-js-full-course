f9709c34369d63f4781c6e78de58746a1a7222ea
sudhanshu ranjan <166327067+Mithu699@users.noreply.github.com>
2026-07-16 20:56:10 +0530

    Delete node-package-manager directory

--- node-package-manager/index.js
+++
@@ -1,6 +0,0 @@
-const lodash = require("lodash");
-
-const names = ["sudhanshu", "saggy ", "john", "bucther", "mia"];
-
-const capitalize = lodash.map(names, lodash.upperCase);
-console.log(capitalize);
