ca61d5f48954b4c551a5b4b9455dec7c1f2cb000
sudhanshu ranjan <166327067+Mithu699@users.noreply.github.com>
2026-07-16 21:03:36 +0530

    Add files via upload

---
+++ 09-async-await/index.js
@@ -0,0 +1,30 @@
+function delayFn(time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
+}
+
+async function delayedGreet(name) {
+  await delayFn(2000);
+  console.log(name);
+}
+
+delayedGreet("sudhanshu");
+
+async function division(num1, num2) {
+  try {
+    if (num2 === 0) {
+      throw new Error("We cannot divide by zero");
+    }
+
+    return num1 / num2;
+  } catch (error) {
+    console.log("Error:", error.message);
+    return null;
+  }
+}
+
+async function mainFn() {
+  console.log(await division(10, 2));
+  console.log(await division(10, 0));
+}
+
+mainFn();
