ea5ed2b52a217babdb10d28c386e7ac5c8b354ea
sudhanshu ranjan <166327067+Mithu699@users.noreply.github.com>
2026-07-16 20:57:46 +0530

    Add files via upload

---
+++ 02-node-module-system/wrapper-demo.js
@@ -0,0 +1,7 @@
+const wrapperExplorer = require("./wrapper-explorer");
+
+console.log(`in demo wrapper demo.js file `);
+console.log("__filename in demo", __filename);
+console.log("__dirname in demo", __dirname);
+
+wrapperExplorer.greet("sudhanshu ranjan ");
